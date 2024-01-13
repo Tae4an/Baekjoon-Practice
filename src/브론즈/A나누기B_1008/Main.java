5a5afdbe2afa687de1e770003d1907128f182d62
Choi TaeSan <xotks7524@gmail.com>
2024-01-13 18:36:41 +0900

    first commit

---
+++ src/브론즈/A나누기B_1008/Main.java
@@ -0,0 +1,16 @@
+package 브론즈.A나누기B_1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+
+        double sum = A / B;
+
+        System.out.println(sum);
+    }
+}
