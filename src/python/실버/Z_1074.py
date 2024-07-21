fce22d143ced589ab3dc6c9238b384e808b6a0af
Choi TaeSan <xotks7524@gmail.com>
2024-07-21 18:02:16 +0900

    first commit

---
+++ src/python/실버/Z_1074.py
@@ -0,0 +1,21 @@
+def z_order(N, r, c):
+    if N == 0:
+        return 0
+    half = 2 ** (N - 1)
+    
+    if r < half and c < half:
+        # 1사분면
+        return z_order(N - 1, r, c)
+    elif r < half and c >= half:
+        # 2사분면
+        return half * half + z_order(N - 1, r, c - half)
+    elif r >= half and c < half:
+        # 3사분면
+        return 2 * half * half + z_order(N - 1, r - half, c)
+    else:
+        # 4사분면
+        return 3 * half * half + z_order(N - 1, r - half, c - half)
+
+N, r, c = map(int, input().split())
+
+print(z_order(N, r, c))
